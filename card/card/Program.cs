db708939512050cbe5d392d185e22283b0af0ce2
joel4899 <73161763+joel4899@users.noreply.github.com>
2023-02-01 17:52:37 +0100

    Changed program
    changed program because it was missing some features

--- card/card/Program.cs
+++ card/card/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace card
 {
@@ -8,44 +9,39 @@ namespace card
     {
         static void Main(string[] args)
         {
-            string input = "TS QC 8S 8D QH 2D 3H KH 9H 2H TH KS KC" +
-                           "\n9D JH 7H JD 2S QS TD 2C 4H 5H AD 4D 5D" +
-                           "\n6D 4S 9S 5S 7S JS 8H 3D 8C 3S 4C 6S 9C" +
-                           "\nAS 7C AH 6H KD JC 7D AC 5C TC QD 6C 3C" +
-                           "\n#";
-            string[] decks = input.Split('#')[0].Split('\n');
-            foreach (string deck in decks)
+            string input = Console.ReadLine();
+            while (input != "#")
             {
-
-                string[] cards = deck.Split(' ');
-                int exposedCards = 0;
-                string lastExposedCard = "";
-                int index = 0;
-
-
-                for (int i = 12; i >= 0; i--)
+                string[] cards = input.Split().Concat(Console.ReadLine().Split())
+                    .Concat(Console.ReadLine().Split())
+                    .Concat(Console.ReadLine().Split())
+                    .ToArray();
+                int[] piles = new int[13];
+                for (int i = 0; i < 13; i++)
                 {
-                    if (i <= cards.Length)
-                    {
-                        if (i + exposedCards >= 12)
-                        {
-                            lastExposedCard = cards[i];
-                            index = Array.IndexOf(cards, lastExposedCard);
-                            Console.WriteLine($"{index:D2},{lastExposedCard}");
-                            Console.ReadLine();
-
-                        }
-                       
-
-
-
-
-                    }
+                    piles[i] = 4;
                 }
+                int currentPile = 12;
+                int exposed = 4;
+                string lastExposed = null;
+                for (int i = cards.Length - 1; i >= 0; i--)
+                {
+                    //int rank = "23456789TJQKA".IndexOf(cards[i][0]);
+                    piles[currentPile]--;
+                    exposed++;
+                    lastExposed = cards[exposed - 1];
+                    if (piles[currentPile] == 0)
+                    {
+                        break;
+                    }
 
-                
+                    currentPile = (currentPile + 1) % 13;
+                }
+                Console.WriteLine("{0:D2},{1}", exposed, lastExposed);
+                input = Console.ReadLine();
             }
-
         }
-        }
+    }
 }
+
+
